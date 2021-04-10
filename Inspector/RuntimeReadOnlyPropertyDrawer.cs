6014809cbb59a7325629b70686888417e68192ea
piotr <piotrekp94@gmail.com>
2021-04-10 16:58:48 +0200

    Add ReadOnly Script

--- Inspector/RuntimeReadOnlyPropertyDrawer.cs
+++ Inspector/RuntimeReadOnlyPropertyDrawer.cs
@@ -3,21 +3,18 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Tools.Inspector
+[CustomPropertyDrawer(typeof(RuntimeReadOnlyAttribute))]
+public class RuntimeReadOnlyPropertyDrawer : PropertyDrawer
 {
-    [CustomPropertyDrawer(typeof(RuntimeReadOnlyAttribute))]
-    public class RuntimeReadOnlyPropertyDrawer : PropertyDrawer
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            return EditorGUI.GetPropertyHeight(property, label, true);
-        }
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
 
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            GUI.enabled = !Application.isPlaying;
-            EditorGUI.PropertyField(position, property, label, true);
-            GUI.enabled = true;
-        }
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = !Application.isPlaying;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
     }
 }
